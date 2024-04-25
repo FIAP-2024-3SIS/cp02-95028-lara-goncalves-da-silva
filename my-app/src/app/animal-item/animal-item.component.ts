8f2715996a8ddd6c83f8d1db17676836119f6656
lara <lara>
2024-04-24 21:03:04 -0300

    add class animal

---
+++ my-app/src/app/animal-item/animal-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-item.component.html',
+  styleUrl: './animal-item.component.css'
+})
+export class AnimalItemComponent {
+
+}
---
+++ my-app/src/app/models/animal.ts
@@ -0,0 +1,8 @@
+class Animal{
+
+  public nome: string = "";
+  public especie: string = "";
+  public idade: number = 0;
+  public sexo: string = "";
+
+}
